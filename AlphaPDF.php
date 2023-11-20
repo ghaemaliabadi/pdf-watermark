6e35396a411889aab93119833d9b5d1a82125394
ghaem <ghaemaliabadi@gmail.com>
2023-11-20 23:30:49 +0330

    chore: AlphaPDF class added.

---
+++ AlphaPDF.php
@@ -0,0 +1,65 @@
+<?php
+
+
+use setasign\Fpdi\Fpdi;
+
+class AlphaPDF extends FPDI
+{
+    protected $extgstates = array();
+
+    function SetAlpha($alpha, $bm = 'Normal')
+    {
+        // set alpha for stroking (CA) and non-stroking (ca) operations
+        $gs = $this->AddExtGState(array('ca' => $alpha, 'CA' => $alpha, 'BM' => '/' . $bm));
+        $this->SetExtGState($gs);
+    }
+
+    function AddExtGState($parms): int
+    {
+        $n = count($this->extgstates) + 1;
+        $this->extgstates[$n]['parms'] = $parms;
+        return $n;
+    }
+
+    function SetExtGState($gs)
+    {
+        $this->_out(sprintf('/GS%d gs', $gs));
+    }
+
+    function _enddoc()
+    {
+        if (!empty($this->extgstates) && $this->PDFVersion < '1.4')
+            $this->PDFVersion = '1.4';
+        parent::_enddoc();
+    }
+
+    function _PutExtGStates()
+    {
+        for ($i = 1; $i <= count($this->extgstates); $i++) {
+            $this->_newobj();
+            $this->extgstates[$i]['n'] = $this->n;
+            $this->_put('<</Type /ExtGState');
+            $parms = $this->extgstates[$i]['parms'];
+            $this->_put(sprintf('/ca %.3F', $parms['ca']));
+            $this->_put(sprintf('/CA %.3F', $parms['CA']));
+            $this->_put('/BM ' . $parms['BM']);
+            $this->_put('>>');
+            $this->_put('endobj');
+        }
+    }
+
+    function _putResourceDict()
+    {
+        parent::_putresourcedict();
+        $this->_put('/ExtGState <<');
+        foreach ($this->extgstates as $k => $extgstate)
+            $this->_put('/GS' . $k . ' ' . $extgstate['n'] . ' 0 R');
+        $this->_put('>>');
+    }
+
+    function _putResources()
+    {
+        $this->_PutExtGStates();
+        parent::_putresources();
+    }
+}
