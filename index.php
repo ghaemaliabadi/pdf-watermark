3c6967537ae32adb9d9f31cf169ec124ca29b996
ghaem <ghaemaliabadi@gmail.com>
2023-11-21 00:05:42 +0330

    chore: some comments and the ability to delete border added

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 require 'AlphaPDF.php';
 ////######## CONFIG #########/////
-$fileName = 'input.pdf';
+$inputFileName = 'input.pdf';
 $watermarkImage = 'logo.jpg';
-$borderImage = 'border.png';
+$borderImage = 'border.png'; // Optional, if you don't want to use border for your PDF, comment this line
 $outputFileName = 'output.pdf';
+$watermarkOpacity = 20; // Range: 0-100
 ////######## SCRIPT #########/////
 $pdf = new AlphaPDF();
-if (file_exists("./" . $fileName)) {
+if (file_exists("./" . $inputFileName)) {
     try {
-        $pageCount = $pdf->setSourceFile($fileName);
+        $pageCount = $pdf->setSourceFile($inputFileName);
     } catch (Exception $e) {
         die('Error occurred during file loading. Exception:' . $e->getMessage());
     }
@@ -26,11 +27,14 @@ for ($i = 1; $i <= $pageCount; $i++) {
     $size = $pdf->getTemplateSize($tpl);
     $pdf->addPage();
     $pdf->useTemplate($tpl, 1, 1, $size['width'], $size['height'], TRUE);
-    $pdf->SetAlpha(0.2);
-    $xFinal = ($size['width'] / 2 - 45);
-    $yFinal = ($size['height'] / 2 - 40);
+    $pdf->SetAlpha($watermarkOpacity / 100);
+    $xFinal = ($size['width'] / 2 - 45); // Change this value to move the watermark to right or left
+    $yFinal = ($size['height'] / 2 - 40); // Change this value to move the watermark to top or bottom
     $pdf->Image($watermarkImage, $xFinal, $yFinal, 0, 0, 'jpg');
-    $pdf->SetAlpha(1);
-    $pdf->Image($borderImage, 0, 0, $size['width'], $size['height'], 'png');
+    /** @noinspection PhpConditionAlreadyCheckedInspection */
+    if (isset($borderImage)) {
+        $pdf->SetAlpha(1);
+        $pdf->Image($borderImage, 0, 0, $size['width'], $size['height'], 'png');
+    }
 }
 $pdf->Output('F', $outputFileName);
